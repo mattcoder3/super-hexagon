2153024a7e3a45bbb4e86b485c55e16fbafbc32e
jbarcossc <juancruzbarcos@hotmail.com>
2024-05-13 01:25:10 -0300

    Deploy

---
+++ src/scripts/api/config.js
@@ -0,0 +1,5 @@
+export const apicoIntegrationId = 'x4M7yW';
+export const spreadSheetId =
+	'1yGRz8aMu17hWvSdxCHcUA3AncNnTNo-5SJcKzCXsVVw';
+export const sheetName = 'Scores'; // replace with your sheet name
+export const sheetId = 0; // replace with your sheet/page gid (not sheet name)
